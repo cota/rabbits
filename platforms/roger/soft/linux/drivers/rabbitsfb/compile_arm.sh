42fa4dd2935771986ce245c5773cdfe22cd507f1
Nicolas Fournel <nicolas.fournel@imag.fr>
2010-11-24 15:49:49 +0100

    * Missing Headers

--- platforms/roger/soft/linux/drivers/rabbitsfb/compile_arm.sh
+++ platforms/roger/soft/linux/drivers/rabbitsfb/compile_arm.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Copyright (c) 2010 TIMA Laboratory
+#
+# This file is part of Rabbits.
+#
+# Rabbits is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rabbits is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Rabbits.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 HERE=`pwd`
 
