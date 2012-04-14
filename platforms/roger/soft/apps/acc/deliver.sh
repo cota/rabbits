802600eca70bc0ab7cb99ec28212daad5bcbbb23
Emilio G. Cota <cota@braap.org>
2012-04-14 01:49:07 -0400

    roger/acc: deal with huge images separately
    Given that they take ~45s to run on the panda board,
    run them manually if desired.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- platforms/roger/soft/apps/acc/deliver.sh
+++ platforms/roger/soft/apps/acc/deliver.sh
@@ -7,6 +7,13 @@ INITRD_DIR=/home/cota/src/rabbits/platforms/roger/soft/initrd
 
 make
 ./makeall.sh
+
+for img in heart411 earth411 stars411; do
+    for type in code0 jpeg; do
+	mv out.$type.$img out.slow.$type.$img
+    done
+done
+
 cp -v load.sh runall.sh runsoft.sh runacc.sh out.* n_cycles $DEST
 cd $INITRD_DIR
 ./make_initramfs.sh
